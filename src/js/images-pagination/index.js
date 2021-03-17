ede8b434f844b9a33f4b72525d3413ab946c676c
Ania Walker <ania.wlkr@gmail.com>
2021-03-17 21:22:22 +0200

    changed scroll settings

--- src/js/images-pagination/index.js
+++ src/js/images-pagination/index.js
@@ -10,11 +10,28 @@ class imagesPagination {
   constructor() {
     this.currentPage = 1;
     this.queryStr = '';
-    this.scrollMarker = null;
+    // this.scrollMarker = null;
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.loadMore = this.loadMore.bind(this);
   }
   
+  onFormSubmit(event) { 
+    event.preventDefault();
+    this.currentPage = 1; 
+    refs.gallery.innerHTML = '';
+
+    this.queryStr = refs.form.query.value.trim();
+    if (!this.queryStr) return;
+
+    this.fetchImages();
+  }
+   
+  fetchImages() { 
+    return imagesApi(this.currentPage, this.queryStr)
+    .then(this.renderImages)
+    .catch(err => onFetchError(err));
+  }
+
   renderImages({ hits }) { 
     if (hits.length === 0) {
       refs.loadMoreBtn.classList.remove('is-visible');
@@ -26,32 +43,27 @@ class imagesPagination {
     refs.loadMoreBtn.classList.add('is-visible');
   }
   
-  fetchImages() { 
-    return imagesApi(this.currentPage, this.queryStr)
-    .then(this.renderImages)
-    .catch(err => onFetchError(err));
-  }
-  
   loadMore() { 
     this.currentPage += 1;
-    this.scrollMarker = refs.gallery.lastElementChild;
-    
+    // this.scrollMarker = refs.gallery.lastElementChild;
+   
     this.fetchImages()
-      .then(() =>
-    this.scrollMarker.scrollIntoView({behavior: "smooth" }));
+      .then(() => this.scrollToNewElements()
+    // this.scrollMarker.scrollIntoView({behavior: "smooth"})
+    );
   }
 
-  onFormSubmit(event) { 
-    event.preventDefault();
-    this.currentPage = 1; 
-    refs.gallery.innerHTML = '';
-
-    this.queryStr = refs.form.query.value.trim();
-    if (!this.queryStr) return;
-
-    this.fetchImages();
-  }
+  scrollToNewElements() {
+    const headerHeight = document.querySelector('.pixabay-link').clientHeight + document.querySelector('#search-form').clientHeight;
+    const totalScrollHeight = refs.gallery.clientHeight - headerHeight - 40;
+ 
+    setTimeout(() => {
+      window.scrollTo({
+        top: totalScrollHeight,
+        behavior: 'smooth',
+      });
+    }, 500);
+}
 }
 
 export default imagesPagination;
-
